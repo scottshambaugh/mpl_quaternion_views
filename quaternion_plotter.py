6de348800563c4f34e69c0d4171d0fc476cacbb3
Scott Shambaugh <scottshambaugh@users.noreply.github.com>
2022-08-23 17:20:08 -0600

    Add inverse quat transform

--- quaternion_plotter.py
+++ quaternion_plotter.py
@@ -19,7 +19,7 @@ def wrap_angle(ang):
 def quat_to_elev_azim_roll(q, angle_offsets=(0, 0, 0)):
     # See Diebel, James "Representing Attitude: Euler Angles, Unit Quaternions, and Rotation Vectors" (2006)
     # https://www.astro.rug.nl/software/kapteyn-beta/_downloads/attitude.pdf
-    # Sequence 321
+    # Sequence (3, 2, 1), Eqn. 452
     q0, q1, q2, q3 = q.w, q.x, q.y, q.z
     phi = np.arctan2(-2*q1*q2 + 2*q0*q3, q1**2 + q0**2 - q3**2 - q2**2)
     theta = np.arcsin(2*q1*q3 + 2*q0*q2)
@@ -28,6 +28,21 @@ def quat_to_elev_azim_roll(q, angle_offsets=(0, 0, 0)):
     elev = -np.rad2deg(theta) + angle_offsets[1]
     roll = np.rad2deg(psi) + angle_offsets[2]
     return elev, azim, roll
+
+
+def elev_azim_roll_to_quat(elev, azim, roll, angle_offsets=(0, 0, 0)):
+    # See Diebel, James "Representing Attitude: Euler Angles, Unit Quaternions, and Rotation Vectors" (2006)
+    # https://www.astro.rug.nl/software/kapteyn-beta/_downloads/attitude.pdf
+    # Sequence (3, 2, 1), Eqn. 459
+    phi = np.deg2rad(azim) - angle_offsets[0]
+    theta = np.deg2rad(-elev) - angle_offsets[1]
+    psi = np.deg2rad(roll) - angle_offsets[2]
+    q0 = np.cos(phi/2)*np.cos(theta/2)*np.cos(psi/2) - np.sin(phi/2)*np.sin(theta/2)*np.sin(psi/2)
+    q1 = np.cos(phi/2)*np.cos(theta/2)*np.sin(psi/2) + np.sin(phi/2)*np.sin(theta/2)*np.cos(psi/2)
+    q2 = np.cos(phi/2)*np.sin(theta/2)*np.cos(psi/2) - np.sin(phi/2)*np.cos(theta/2)*np.sin(psi/2)
+    q3 = np.cos(phi/2)*np.sin(theta/2)*np.sin(psi/2) + np.sin(phi/2)*np.cos(theta/2)*np.cos(psi/2)
+    q = np.quaternion(q0, q1, q2, q3)
+    return q
 
 
 class quaternion_plotter():
